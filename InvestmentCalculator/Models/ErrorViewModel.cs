cfef8f79a3ab0ab1838bf626bfa57cf56d127a44
Abinaya-meganathan <abinayameganathan5@gmail.com>
2026-03-01 20:51:11 +0530

    changed

--- InvestmentCalculator/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace InvestmentCalculator.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
